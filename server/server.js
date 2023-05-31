2716fa31a7d054756265a36e68957b4cad49b11f
Elroy Chua <elroymx@gmail.com>
2023-06-01 00:59:11 +0800

    added server component changes

--- server/server.js
+++ server/server.js
@@ -5,13 +5,17 @@ const app = express();
 const pool = require("./db");
 //Get all todos
 app.get("/todos", async (req, res) => {
+  const userEmail = "elroy@test.com";
   try {
-    const todos = await pool.query("SELECT * FROM todos");
+    const todos = await pool.query("SELECT * FROM todos WHERE user_email= $1", [
+      userEmail,
+    ]);
     res.json(todos.rows);
   } catch (err) {
     console.log(err);
   }
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 });
